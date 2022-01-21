d024f98bc7da9fe26cdf9a5fd250badf8cb55507
zhangkang <zhangkang@feewee.cn>
2022-01-21 18:21:07 +0800

    评论最大宽度

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -113,6 +113,7 @@ const Article: React.FC = () => {
       <div className="comment">
         {commentList.map((e: CommentList) => (
           <Comment
+            style={{maxWidth: 800}}
             key={e.commentId}
             actions={[
               <span key="comment-nested-reply-to">
@@ -147,6 +148,7 @@ const Article: React.FC = () => {
                 style={{
                   background: "rgba(247,248,250,.7)",
                   paddingInline: 20,
+                  maxWidth: 800
                 }}
                 author={
                   <span
